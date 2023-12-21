53c3a3fab21b096983ebb879b31ff3fbba5aeaa7
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-12-21 13:23:35 +0300

    Test: findAll on controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,13 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.findAll()).toBe(
+        'Hello World! <br>' +
+          'This is a test of the emergency broadcast system. <br>' +
+          'This is only a test. <br>' +
+          'Had this been an actual emergency, you would have been instructed where to tune in your area for news and official information. <br>' +
+          'This concludes this test of the emergency broadcast system.',
+      );
     });
   });
 });
