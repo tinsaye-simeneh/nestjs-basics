d446dff0c33b984173dd71d8502574fb41309d94
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-12-21 13:17:20 +0300

    update: service test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return (
+      'Hello World! <br>' +
+      'This is a test of the emergency broadcast system. <br>' +
+      'This is only a test. <br>' +
+      'Had this been an actual emergency, you would have been instructed where to tune in your area for news and official information. <br>' +
+      'This concludes this test of the emergency broadcast system.'
+    );
   }
 }
